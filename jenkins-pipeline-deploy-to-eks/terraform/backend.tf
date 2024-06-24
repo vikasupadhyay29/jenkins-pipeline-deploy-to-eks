9af72e3978c5d814f51a215b82a3604a683d71d7
vikasupadhyay29 <vikasupadhyay.2012@gmail.com>
2024-06-24 08:23:24 +0530

    uploading code

--- jenkins-pipeline-deploy-to-eks/terraform/backend.tf
+++ jenkins-pipeline-deploy-to-eks/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
-    region = "us-east-1"
+    bucket = "vikaslearning-app"
+    region = "ap-south-1"
     key = "eks/terraform.tfstate"
   }
 }
